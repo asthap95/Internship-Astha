70c1be00d9be034e6b52479448eeadbf81f9efc9
asthap95 <121311485+asthap95@users.noreply.github.com>
2023-10-09 00:43:42 +0530

    Add files via upload

---
+++ glass identification.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pandas as pd
+import numpy as nb
+import seaborn as sns
+import matplotlib as plt
+import os
+
+
+# In[7]:
+
+
+df = pd.read_csv('glass.csv')
+
+
+# In[8]:
+
+
+df.head()
+
+
+# In[10]:
+
+
+df.info()
+
+
+# In[12]:
+
+
+df.describe()
+
+
+# In[ ]:
+
+
+
+
