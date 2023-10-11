30006f39a1cf98ae42d73461e81b3c0eeb326656
asthap95 <121311485+asthap95@users.noreply.github.com>
2023-10-11 23:20:31 +0530

    Add files via upload

---
+++ student grade prediction-Copy1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+
+# In[13]:
+
+
+data= pd.read_csv("students.csv")
+
+
+# In[14]:
+
+
+data.head()
+
+
+# In[17]:
+
+
+data.describe()
+
+
+# In[16]:
+
+
+data.info()
+
+
+# In[ ]:
+
+
+
+
