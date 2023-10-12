bb2a39b8ecee96fc0458ae795032b3bb4610472b
asthap95 <121311485+asthap95@users.noreply.github.com>
2023-10-12 19:23:45 +0530

    Add files via upload

---
+++ bank term deposit prediction.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sb
+import matplotlib as plt
+
+
+# In[5]:
+
+
+df=pd.read_csv('bank.csv')
+
+
+# In[6]:
+
+
+df.info()
+
+
+# In[7]:
+
+
+df.head()
+
+
+# In[8]:
+
+
+df.describe()
+
+
+# In[ ]:
+
+
+
+
